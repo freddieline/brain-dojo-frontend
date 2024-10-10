f594acb6cb2ddab250277ae32a70e98462bc062c
Freddie Line <freddieline@hotmail.com>
2024-10-10 10:10:02 +0100

    fix: remove dark theme

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,5 +1,5 @@
 const Layout: React.FC<any> = ({ children }) => {
-  return <div className="App max-w-[550px] m-auto p-5 mt-5">{children}</div>
+  return <div className="max-w-[550px] m-auto p-5 mt-5">{children}</div>
 }
 
 export default Layout
