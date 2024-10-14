e445d202257d967a9849408850b40bdb082847db
Freddie Line <freddieline@hotmail.com>
2024-10-14 15:04:29 +0100

    fix: update wording

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -9,7 +9,7 @@ const Home = () => {
 			<div className={`w-[100%] overflow-hidden shadow-lg`}>
       	<img className=" object-cover" src="babel.png" alt="Tower of Babel" />
     	</div>
-			<p className="text-lg  mt-5 mb-5">I've always been interested in learning new bits of knowledge from a range of topics. Hope you enjoy the quizzes I've created below :-)</p>
+			<p className="text-lg  mt-5 mb-5">I've always been interested in learning new bits of knowledge from a range of topics. Whether it's about the furthest reaches of space or fallen civilizations from long ago. Hope you enjoy the quizzes and take something away with you :-)</p>
 			<p className="text-lg  mt-5 mb-5">Please leave some feedback at the end too. This will really help me in making the quizzes better.</p>
 
 			<Link className="w-48 underline" to="/scale-in-science">
