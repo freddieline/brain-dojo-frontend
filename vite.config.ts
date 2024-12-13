8556f7eb1707a852f54ed8c7f06ba33e1d157e83
Freddie Line <freddieline@hotmail.com>
2024-12-13 13:55:46 +0000

    fix: prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import ViteEnvironmentPlugin from 'vite-plugin-environment';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import ViteEnvironmentPlugin from "vite-plugin-environment";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    target: 'es2020',
+    target: "es2020",
     rollupOptions: {
-      external: ['msw'],
+      external: ["msw"],
     },
   },
-  plugins: [react(),     ViteEnvironmentPlugin({
-    // Here you can define your env variables you want to inject into your app
-    VITE_QUIZ_API: process.env.VITE_QUIZ_API,
-    MODE: process.env.MODE || 'development', // You can provide default values if desired
-  }),],
-})
+  plugins: [
+    react(),
+    ViteEnvironmentPlugin({
+      // Here you can define your env variables you want to inject into your app
+      VITE_QUIZ_API: process.env.VITE_QUIZ_API,
+      MODE: process.env.MODE || "development", // You can provide default values if desired
+    }),
+  ],
+});
