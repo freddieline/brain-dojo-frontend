2bcb5e4cdf1e3e9551fc4a096ad060d9e33aad61
Freddie Line <freddieline@hotmail.com>
2025-01-10 16:10:56 +0000

    fix: add words to the start

--- src/Home.tsx
+++ src/Home.tsx
@@ -10,13 +10,13 @@ const Home = () => {
           <div className={`w-[100%] overflow-hidden`}>
             <img src="brain-dojo.png" alt="Logo" />
           </div>
-          <Link className="w-48" to="/memory-games">
-            <h3 className="text-xl underline mt-6 decoration-1">
+          <Link to="/memory-games" className="w-48 no-underline text-black hover:underline">
+            <h3 className="text-xl mt-6">
               Memory games
             </h3>
           </Link>
-          <Link className="w-48" to="/word-games">
-            <h3 className="text-xl underline mt-6 decoration-1">Word games</h3>
+          <Link to="/word-games" className="w-48 no-underline text-black hover:underline">
+            <h3 className="text-xl mt-6">Word games</h3>
           </Link>
         </div>
         <div className="fixed bottom-0 left-0 p-3">created by Freddie Line</div>
