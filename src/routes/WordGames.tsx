4f8baab0da790759bbf408b9eb598b1d149a723f
Freddie Line <freddieline@hotmail.com>
2025-01-10 16:52:08 +0000

    fix: add link

--- src/routes/WordGames.tsx
+++ src/routes/WordGames.tsx
@@ -5,15 +5,23 @@ import { Size } from "../types/constants";
 const WordGames = () => {
   return (
     <Layout size={Size.small}>
-      <div className={`w-[100%] overflow-hidden`}>
-        <img src="brain-dojo.png" alt="Logo" />
+      <div>
+        <div className="h-[82vh]">
+          <div className={`w-[100%] overflow-hidden`}>
+            <img src="brain-dojo.png" alt="Logo" />
+          </div>
+          <div className="mt-6 font-bold">
+            <a href="/">Home</a> {">"} Word Games
+          </div>
+          <Link to="/word-games/word-wheel" className="no-underline text-black hover:underline">
+            <h3 className="text-xl mt-3">Word Wheel</h3>
+          </Link>
+        </div>
+        <a href="https://buymeacoffee.com/freddieline">
+          <img src="bmc-full-logo.png" width={200} className="mb-2 hover:pointer" />
+        </a>
+        created by Freddie Line
       </div>
-      <div className="mt-6 font-bold">
-        <a href="/">Home</a> {">"} Word Games
-      </div>
-      <Link to="/word-games/word-wheel" className="no-underline text-black hover:underline">
-        <h3 className="text-xl mt-3">Word Wheel</h3>
-      </Link>
     </Layout>
   );
 };
